508a8d8f5083f26a96bd91df508bb44c3eab711a
John Young <jyoung@gol.com>
2022-10-08 14:59:11 +0900

    MFCX-709: Add version file

---
+++ Version.h
@@ -0,0 +1,21 @@
+//
+//   Version.h   Version file for the MFCX project
+//
+//	Ver Date       Bld  Description
+//	--------------------------------------------------------------------------
+//	0.1 2022-10-08   1 Initial build
+//
+//	+ means an added feature or enhancement
+//	- means bug fix
+//	! change to existing feature
+//	. notice - little or no change to the functionality
+//
+
+// Version numbers below here get updated by the tag-build.sh script - DO NOT EDIT
+#define   VER_MAJOR      0
+#define   VER_MINOR      1
+#define   VER_REVISION   0
+#define   VER_BUILD      0
+#define   VER_STRING     "0, 1, 0, 0\0"
+#define   VER_BUILDDATE  "28 November 2019\0"
+#define   VER_BUILDINFO  "jyoung@WOOSTER 14:57 2022-10-08\0"
